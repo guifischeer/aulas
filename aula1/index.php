6bc64745ba448901e6d0a1442fc6bd144ca21813
guifischeer <guilherme_florent@estudante.sesisenai.org.br>
2025-08-26 09:17:39 -0300

    criacao

---
+++ aula1/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$mensagem = 'Olá, mundo!';
+echo "<p>$mensagem</p>"; 
+echo '<p>' . $mensagem . '</p>'
+
+$primeiro_nome = 'Guilherme',
+$idade = 23,
+$altura = 1.96,
+$idade = (int) 1.96;
+
+$gosta_de_bolo = true;
+
+?>
